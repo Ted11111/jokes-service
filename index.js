6ee8bc1c192dbc0e6a9af83fe921d89a3e5306ae
Ted11111 <teddyslight@gmail.com>
2023-04-05 11:59:29 +0100

    Completed Tasks

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 const express = require('express');
 const app = express();
 const { Joke } = require('./db');
-
+const { Op } = require('sequelize')
+app.set("json spaces", "\t")
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+
 
 app.get('/jokes', async (req, res, next) => {
+  if (req.query) {
+    try{
+      const jokes = await Joke.findAll({
+        where: {
+          tags: {
+            [Op.substring]: req.query.tags ?? "",
+          },
+          joke: {
+            [Op.substring]: req.query.content ??  "",
+          },
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      next(error)
+    }
+  }
   try {
-    // TODO - filter the jokes by tags and content
-    const jokes = [];
-    res.send(jokes);
+    const jokes = await Joke.findAll()
+    res.send(jokes)
   } catch (error) {
     console.error(error);
     next(error)
   }
 });
+
+app.post("/jokes", async (req, res, next) => {
+  try {
+      const joke = await Joke.create(req.body);
+      res.status(201).send(joke);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+})
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
